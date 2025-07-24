fc9eec03088ed5974fff2d59b46ffb25e615553b
fayzillo95 <ovovovlululutvata@gmail.com>
2025-07-24 19:35:36 +0500

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix("api")
   const document = new DocumentBuilder()
-    .setTitle('UzMovie.com')
-    .addBearerAuth()
+    .setTitle('Books')
     .build();
 
   const config = SwaggerModule.createDocument(app, document);
   SwaggerModule.setup('/api-docs', app, config);
   
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist : true,
+    forbidNonWhitelisted :true,
+    forbidUnknownValues : true,
+    transform : true
+  }))
+  app.enableCors()
+  
   const HOST = process.env.HOST || "localhost"
   const PORT = process.env.PORT ?? 3000
-  const BASE_URL = `http://${HOST}:${PORT}`
+  const BASE_URL = process.env.BASE_URL || `http://${HOST}:${PORT}` // baseur
 
   await app.listen(process.env.PORT ?? 3000);
   console.log(
-    `Server is running ✅✅✅  \n${BASE_URL}/api-docs`,
+    ` Server is running ✅✅✅  \n ${BASE_URL}/api-docs\n`,
+    process.env.BASE_URL
   );
 }
 bootstrap();
